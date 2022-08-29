5dceb4c23576b80978032a5bcf6911480e0f0f67
miguelvalero <miguel.valero@upc.edu>
2022-08-29 10:56:22 +0200

    Step4: BootstrapVue buttons and input

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -1,18 +1,48 @@
 <template>
     <div class="topStyle">
-        Top
-
+        <div style = "margin-left: 20%">
+       
+            <b-button @click="primaryClicked" style="margin: 1%; width:15%" variant="primary">Primary</b-button>
+            <b-button style="margin: 1%; width:15%" variant="secondary">Secondary</b-button>
+            <b-button style="margin: 1%; width:15%" variant="success">Success</b-button>
+            <b-button style="margin: 1%; width:15%" variant="danger">Danger</b-button>
+     
+        </div>
+        <b-input-group prepend="New user" style = "width:50%; margin-left: 22%; margin-top: 1%">
+            <b-form-input placeholder="name here" v-model="username"></b-form-input>
+            <b-form-input placeholder="age here" v-model="age"></b-form-input>
+            <b-input-group-append>
+            <b-button @click ="InputUsername" variant="info">Enter</b-button>
+            </b-input-group-append>
+        </b-input-group>
     </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
     setup () {
+        let username = ref(undefined);
+        let age= ref(undefined);
+        function primaryClicked () {
+            console.log ('primary clicled')
+        }
+        function InputUsername () {
+            console.log ('name: ', username.value, ' age: ', age.value);
+            username.value = undefined;
+            age.value = undefined;
+
+
+        }
         
 
-        return {}
+        return {
+            primaryClicked,
+            InputUsername,
+            username,
+            age
+        }
     }
 })
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// https://cdmoro.github.io/bootstrap-vue-3/
+import BootstrapVue3 from 'bootstrap-vue-3'
+
+// Optional, since every component import their Bootstrap functionality
+// the following line is not necessary
+// import 'bootstrap'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+
+const app = createApp(App)
+app.use(BootstrapVue3)
+app.mount('#app')
