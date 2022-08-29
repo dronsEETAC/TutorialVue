2c912183de0abb47093ad0d361fffb316d73df20
miguelvalero <miguel.valero@upc.edu>
2022-08-29 09:56:19 +0200

    Step1: demo vue structure

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  
+  <HelloWorld msg="Hello from parent"/>
+  
 </template>
 
 <script lang="ts">
