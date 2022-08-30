7ad003552762194ec3c39412fdd36d768d883e7e
miguelvalero <miguel.valero@upc.edu>
2022-08-30 10:56:42 +0200

    Step11: Video stream

--- src/components/Left.vue
+++ src/components/Left.vue
@@ -1,17 +1,64 @@
 <template>
     <div class = "leftStyle">
-        Left
+        <div>
+        <b-button style ="width:20%; margin:1%; margin-left:20%"  @click="startVideoFrame" variant="success">
+                Start video frame 
+        </b-button>
+         <b-button style ="width:20%; margin:1%" @click="stopVideoFrame" variant="warning">
+                Stop video frame 
+        </b-button>
+        </div>
+        <div  style ="width:70%">
+        <canvas style="margin-left:20%; width: 400px; height: 300px; border-style: solid;" id= "output"></canvas>
+        </div> 
     </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script>
+import { onMounted,defineComponent, inject } from 'vue'
 
 export default defineComponent({
     setup () {
+        let client = inject('mqttClient');
+        onMounted(() => {
+            client.on('message', (topic, message) => {
+                if (topic == 'videoFrame') {
+                    const img = new Image();
+                    img.src = "data:image/jpg;base64,"+message;
+                    const canvas = document.getElementById('output'); 
+                    const context = canvas.getContext('2d');      
+                    img.onload = () => {          
+                        context.drawImage(
+                            img,
+                            0,
+                            0,
+                            img.width,
+                            img.height,
+                            0,
+                            0,
+                            canvas.width,
+                            canvas.height
+                            );  
+                    };
+                   
+                }
+            })
+        })
         
 
-        return {}
+        function startVideoFrame () {
+            client.publish ("StartVideoStream")
+            client.subscribe("videoFrame");
+        }
+         function stopVideoFrame () {
+            client.publish ("StopVideoStream")
+        }
+
+        return {
+            startVideoFrame,
+            stopVideoFrame,
+            client
+        }
     }
 })
 </script>
